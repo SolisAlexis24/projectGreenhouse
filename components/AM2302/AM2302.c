5ec8abf7f1f75abf22d4052b2c1132dddba22cd8
SolisAlexis24 <ianalexis9137@gmail.com>
2025-11-02 19:37:17 -0600

    Se ocultan las funciones internas del modulo

--- components/AM2302/AM2302.c
+++ components/AM2302/AM2302.c
@@ -1,6 +1,4 @@
 #include "AM2302.h"
-#include "portmacro.h"
-#include <time.h>
 
 esp_err_t AM2302init(AM2302Handler *sh, gpio_num_t pin){
 	if(!sh)
@@ -20,38 +18,76 @@ esp_err_t AM2302init(AM2302Handler *sh, gpio_num_t pin){
 }
 
 
-esp_err_t AM2302read(AM2302Handler* sh){
-	if(!sh)
-		return ESP_ERR_INVALID_ARG;
-	if(!sh->readyToUse)
-		return ESP_ERR_INVALID_ARG;
-	
-	esp_err_t error_state;
-
-	vPortEnterCritical(&sh->Spinlock);
-	_AM2302sendStartSignal(sh);
-	_AM2302relaseBus(sh);
-
-	error_state = _AM2302AwaitPinLevel_us(sh, NO_DURATION_RECORD, LOW_LEVEL, 40);
-	if(error_state)
-		return error_state;
-	
-	error_state = _AM2302AwaitPinLevel_us(sh, NO_DURATION_RECORD, HIGH_LEVEL, 80);
-	if(error_state)
-		return error_state;
-	
-	error_state = _AM2302AwaitPinLevel_us(sh, NO_DURATION_RECORD, LOW_LEVEL, 80);
-	if(error_state)
-		return error_state;
-	
-	error_state = _AM2302fetchData(sh);
-	vPortExitCritical(&sh->Spinlock);
-
-	return error_state;
+/**
+ * @brief      Send start signal to AM2302 sensor: 2 ms low
+ *
+ * @param[in]      sh Sensor Handler
+ * 
+ * @warning    Internal function, do not use
+ */
+static void _AM2302sendStartSignal(AM2302Handler* sh){
+	gpio_set_level(sh->pin, LOW_LEVEL);
+	esp_rom_delay_us(1000);
 }
 
 
-esp_err_t _AM2302fetchData(AM2302Handler* sh){
+/**
+ * @brief      Relase the bus so AM2302 can change pin level to transmit ACK and then data
+ *
+ * @param[in]      sh    Sensor Handler
+ */
+static void _AM2302relaseBus(AM2302Handler* sh){
+	gpio_set_level(sh->pin, HIGH_LEVEL);
+}
+
+
+/**
+ * @brief Waits for the AM2302 data line to reach a specific logic level within a timeout period.
+ *
+ * This function continuously samples the GPIO associated with the AM2302 sensor and waits
+ * until it matches the expected logic level. It delays approximately 2 microseconds per 
+ * iteration to avoid busy-waiting too tightly. If the expected level is detected before the timeout expires, 
+ * the elapsed time is optionally stored in `duration`. Otherwise, the function returns a timeout error.
+ *
+ * @param[in]  sh              Pointer to an initialized AM2302Handler structure.
+ * @param[out] duration        Optional pointer to store the time (in microseconds)
+ *                             elapsed until the expected level was detected. Can be NULL.
+ * @param[in]  expected_level  The logic level to wait for (0 for LOW, 1 for HIGH).
+ * @param[in]  timeout         Maximum time to wait in microseconds before returning an error.
+ *
+ * @return
+ *     - ESP_OK: The expected level was detected within the timeout period.  
+ *     - ESP_ERR_TIMEOUT: The timeout expired before detecting the expected level.  
+ *
+ * @note This function does not measure how long the line remains at the expected level;
+ *       it only measures how long it takes for the level to appear.
+ */
+static esp_err_t _AM2302AwaitPinLevel_us(AM2302Handler* sh, uint8_t *duration, int expected_level, uint8_t timeout){
+	for(uint8_t i = 0; i < timeout; i += MINIMUM_RESOLUTION_DELAY){
+		esp_rom_delay_us(MINIMUM_RESOLUTION_DELAY);
+		if(expected_level == gpio_get_level(sh->pin)){
+			if(duration)
+				*duration = i;
+			return ESP_OK;
+		}
+	}
+	vPortExitCritical(&sh->Spinlock);
+	ESP_LOGE(AM2302_TAG, "Timeout has expired after %u [us] awaiting for the bus to be set to %d", timeout, expected_level);
+	return ESP_ERR_TIMEOUT;
+}
+
+
+/**
+ * @brief      Fetch 40 bits of infotmation after the start signals
+ *
+ * @param      sh[in/out]    Sensor Handler
+ *
+ * @return    
+ * - ESP_OK: 					If data was successfully collected and checksum matchs
+ * - ESP_ERR_TIMEOUT: 			If the program kept waiting more than usual
+ * - ESP_ERR_INVALID_RESPONSE:	If checksum does not match 
+ */
+static esp_err_t _AM2302fetchData(AM2302Handler* sh){
 	uint8_t highLevelDuration;
 	uint8_t lowLevelDuration;
 	uint8_t rawData[5] = {0};
@@ -89,26 +125,33 @@ esp_err_t _AM2302fetchData(AM2302Handler* sh){
     return ESP_OK;
 }
 
-void _AM2302sendStartSignal(AM2302Handler* sh){
-	gpio_set_level(sh->pin, LOW_LEVEL);
-	esp_rom_delay_us(1000);
-}
 
-void _AM2302relaseBus(AM2302Handler* sh){
-	gpio_set_level(sh->pin, HIGH_LEVEL);
-}
+esp_err_t AM2302read(AM2302Handler* sh){
+	if(!sh)
+		return ESP_ERR_INVALID_ARG;
+	if(!sh->readyToUse)
+		return ESP_ERR_INVALID_ARG;
+	
+	esp_err_t error_state;
 
+	vPortEnterCritical(&sh->Spinlock);
+	_AM2302sendStartSignal(sh);
+	_AM2302relaseBus(sh);
 
-esp_err_t _AM2302AwaitPinLevel_us(AM2302Handler* sh, uint8_t *duration, int expected_level, uint8_t timeout){
-	for(uint8_t i = 0; i < timeout; i += MINIMUM_RESOLUTION_DELAY){
-		esp_rom_delay_us(MINIMUM_RESOLUTION_DELAY);
-		if(expected_level == gpio_get_level(sh->pin)){
-			if(duration)
-				*duration = i;
-			return ESP_OK;
-		}
-	}
+	error_state = _AM2302AwaitPinLevel_us(sh, NO_DURATION_RECORD, LOW_LEVEL, 40);
+	if(error_state)
+		return error_state;
+	
+	error_state = _AM2302AwaitPinLevel_us(sh, NO_DURATION_RECORD, HIGH_LEVEL, 80);
+	if(error_state)
+		return error_state;
+	
+	error_state = _AM2302AwaitPinLevel_us(sh, NO_DURATION_RECORD, LOW_LEVEL, 80);
+	if(error_state)
+		return error_state;
+	
+	error_state = _AM2302fetchData(sh);
 	vPortExitCritical(&sh->Spinlock);
-	ESP_LOGE(AM2302_TAG, "Timeout has expired after %u [us] awaiting for the bus to be set to %d", timeout, expected_level);
-	return ESP_ERR_TIMEOUT;
+
+	return error_state;
 }
--- components/AM2302/AM2302.h
+++ components/AM2302/AM2302.h
@@ -31,6 +31,16 @@ typedef struct{
 	portMUX_TYPE Spinlock;
 }AM2302Handler;
 
+/**
+ * @brief      Ininialize AM2302 pin
+ *
+ * @param      sh    Sensor handler that will be used with the rest of functions
+ * @param[in]  pin   The pin to be used
+ *
+ * @return
+ * - ESP_OK If given pin got initialized correctly
+ * - ESP_ERR_INVALID_ARG If given pin was not appropiate for initialization or if sh is NULL
+ */
 esp_err_t AM2302init(AM2302Handler *sh, gpio_num_t pin);
 
 /**
@@ -47,55 +57,3 @@ esp_err_t AM2302init(AM2302Handler *sh, gpio_num_t pin);
  * 
  */
 esp_err_t AM2302read(AM2302Handler* sh);
-
-/**
- * @brief      Fetch 40 bits of infotmation after the start signals
- *
- * @param      sh[in/out]    Sensor Handler
- *
- * @return    
- * - ESP_OK: 					If data was successfully collected and checksum matchs
- * - ESP_ERR_TIMEOUT: 			If the program kept waiting more than usual
- * - ESP_ERR_INVALID_RESPONSE:	If checksum does not match 
- */
-esp_err_t _AM2302fetchData(AM2302Handler* sh);
-
-/**
- * @brief      Send start signal to AM2302 sensor: 2 ms low
- *
- * @param[in]      sh Sensor Handler
- * 
- * @warning    Internal function, do not use
- */
-void _AM2302sendStartSignal(AM2302Handler* sh);
-
-/**
- * @brief      Relase the bus so AM2302 can change pin level to transmit ACK and then data
- *
- * @param[in]      sh    Sensor Handler
- */
-void _AM2302relaseBus(AM2302Handler* sh);
-
-
-/**
- * @brief Waits for the AM2302 data line to reach a specific logic level within a timeout period.
- *
- * This function continuously samples the GPIO associated with the AM2302 sensor and waits
- * until it matches the expected logic level. It delays approximately 2 microseconds per 
- * iteration to avoid busy-waiting too tightly. If the expected level is detected before the timeout expires, 
- * the elapsed time is optionally stored in `duration`. Otherwise, the function returns a timeout error.
- *
- * @param[in]  sh              Pointer to an initialized AM2302Handler structure.
- * @param[out] duration        Optional pointer to store the time (in microseconds)
- *                             elapsed until the expected level was detected. Can be NULL.
- * @param[in]  expected_level  The logic level to wait for (0 for LOW, 1 for HIGH).
- * @param[in]  timeout         Maximum time to wait in microseconds before returning an error.
- *
- * @return
- *     - ESP_OK: The expected level was detected within the timeout period.  
- *     - ESP_ERR_TIMEOUT: The timeout expired before detecting the expected level.  
- *
- * @note This function does not measure how long the line remains at the expected level;
- *       it only measures how long it takes for the level to appear.
- */
-esp_err_t _AM2302AwaitPinLevel_us(AM2302Handler* sh, uint8_t *duration, int expected_level, uint8_t timeout);
