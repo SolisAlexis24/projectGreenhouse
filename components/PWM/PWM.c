dad80d4be2ca99660f59c1e5b917bcf80049735f
SolisAlexis24 <ianalexis9137@gmail.com>
2025-11-10 22:57:37 -0600

    Se anade un modulo para controlar ventilador mediante PWM

---
+++ components/PWM/PWM.c
@@ -0,0 +1,84 @@
+/**
+ *************************************
+ * @file: LCD1602.h
+ * @author: Solis Hernandez Ian Alexis
+ * @purpose: Source file for PWM Fan control
+ * @year: 2025
+ * @licence: MIT
+ * ***********************************
+ */
+
+#include "PWM.h"
+#include "driver/ledc.h"
+#include "esp_err.h"
+#include "esp_log.h"
+#include "hal/ledc_types.h"
+#include "soc/clk_tree_defs.h"
+#include <stdbool.h>
+#include <stdint.h>
+
+esp_err_t FanInit(FanHandler *fanHan, gpio_num_t pin, ledc_channel_t channel){
+	if(!fanHan)
+		return ESP_ERR_INVALID_ARG;
+
+	esp_err_t errorStatus;
+	ledc_timer_config_t timer_conf = {
+		.speed_mode = DEF_MODE,
+		.duty_resolution = LEDC_TIMER_11_BIT,
+		.timer_num = LEDC_TIMER_0,
+		.freq_hz = 25000,
+		.clk_cfg = LEDC_AUTO_CLK
+	};
+	errorStatus = ledc_timer_config(&timer_conf);
+	if(errorStatus){
+		ESP_LOGE(PWM_TAG, "Error during configuring timer");
+		return errorStatus;
+	}
+
+	ledc_channel_config_t channel_conf = {
+		.gpio_num = pin,
+		.speed_mode = DEF_MODE,
+		.channel = channel,
+		.timer_sel = LEDC_TIMER_0,
+		.intr_type = LEDC_INTR_DISABLE,
+		.duty = 0,
+		.hpoint = 0
+	};
+	errorStatus = ledc_channel_config(&channel_conf);
+	if(errorStatus){
+		ESP_LOGE(PWM_TAG, "Error during configuring channel");
+		return errorStatus;
+	}
+	errorStatus = ledc_fade_func_install(0);
+	if(errorStatus){
+		ESP_LOGE(PWM_TAG, "Error during fading initialization");
+		return errorStatus;
+	}
+	fanHan->PWMchannel = channel;
+	fanHan->PWMmode = DEF_MODE;
+	return ESP_OK;
+}
+
+esp_err_t setFanDutyCyclePerc(FanHandler *fanHan, float DCpercentage){
+	if(!fanHan)
+		return ESP_ERR_INVALID_ARG;
+
+	uint32_t dutyCycleInt;
+	if(DCpercentage >= 1.0 || DCpercentage <= 0.0)
+		dutyCycleInt = MAX_NUM_11_BITS;
+	else
+		dutyCycleInt = (uint32_t)(MAX_NUM_11_BITS * DCpercentage);
+
+	return ledc_set_duty_and_update(fanHan->PWMmode, fanHan->PWMchannel, dutyCycleInt, 0);
+}
+
+float getFanDutyCyclePerc(FanHandler *fanHan){
+	if(!fanHan)
+		return -1.0;
+
+	uint32_t dutyOutput = ledc_get_duty(fanHan->PWMmode, fanHan->PWMchannel);
+	if(LEDC_ERR_DUTY == dutyOutput)
+		return -1.0;
+
+	return (float)dutyOutput / (float)MAX_NUM_11_BITS;
+}
---
+++ components/PWM/PWM.h
@@ -0,0 +1,64 @@
+/**
+ *************************************
+ * @file: LCD1602.h
+ * @author: Solis Hernandez Ian Alexis
+ * @purpose: Header file for PWM Fan control
+ * @year: 2025
+ * @licence: MIT
+ * ***********************************
+ */
+
+#include "driver/ledc.h"
+#include "esp_err.h"
+#include "hal/ledc_types.h"
+#include "soc/gpio_num.h"
+
+#define DEF_MODE LEDC_LOW_SPEED_MODE
+#define MAX_NUM_11_BITS 2048
+
+static const char *PWM_TAG = "PWM";
+
+
+typedef struct{
+	ledc_channel_t PWMchannel;
+	ledc_mode_t PWMmode;
+}FanHandler;
+
+/**
+ * @brief      Initialize given channel into specified pin and bind such information in the handler
+ *
+ * @param      fanHan   Fan handler which will be binded to the configuration
+ * @param[in]  pin      GPIO pin for PWM
+ * @param[in]  channel  PWM channel
+ *
+ * @return
+ * - ESP_OK Success
+ * - ESP_ERR_INVALID_ARG Parameter error
+ * - ESP_FAIL Can not find a proper pre-divider number base on the given frequency and the current duty_resolution.
+ * - ESP_ERR_INVALID_STATE Timer cannot be de-configured because timer is not configured or is not paused
+ * - ESP_ERR_NOT_FOUND Failed to find available interrupt source
+ */
+esp_err_t FanInit(FanHandler *fanHan, gpio_num_t pin, ledc_channel_t channel);
+
+/**
+ * @brief      Sets the fan duty cycle percentage [0-1]
+ *
+ * @param      fanHan        Fan handler which have an initialized channel
+ * @param[in]  DCpercentage  Duty cycle to be set
+ *
+ * @return
+ * - ESP_OK Success
+ * - ESP_ERR_INVALID_STATE Channel not initialized
+ * - ESP_ERR_INVALID_ARG Parameter error
+ * - ESP_FAIL Fade function init error
+ */
+esp_err_t setFanDutyCyclePerc(FanHandler *fanHan, float DCpercentage);
+
+/**
+ * @brief      Gets the fan duty cycle percentage
+ *
+ * @param      fanHan  Fan handler which have an initialized channel
+ *
+ * @return     Duty cycle if channel is initialized, -1.0 other way
+ */
+float getFanDutyCyclePerc(FanHandler *fanHan);
