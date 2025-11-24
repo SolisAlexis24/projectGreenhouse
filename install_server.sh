e99f42028fa615fb656e17638666dec36f202482
SolisAlexis24 <ianalexis9137@gmail.com>
2025-11-24 10:20:36 -0600

    Modificacion de script para evitar errores en requerimientos del servidor

--- install_server.sh
+++ install_server.sh
@@ -2,19 +2,32 @@
 
 VENV_DIR="Server/venv"
 REQ_FILE="Server/requirements.txt"
+PYTHON_BIN="python3.11"
 
-echo "=== Instalando soporte para entornos virtuales ==="
+echo "=== Actualizando paquetes ==="
 sudo apt update
-sudo apt install -y python3-venv
+
+echo "=== Instalando $PYTHON_BIN ==="
+sudo apt install -y python3.11 python3.11-venv
+
+echo "=== Verificando instalación==="
+if ! command -v $PYTHON_BIN &> /dev/null
+then
+    echo "Error: python3.11 no está instalado."
+    exit 1
+fi
 
 echo "=== Creando entorno virtual ==="
-python3 -m venv "$VENV_DIR"
+$PYTHON_BIN -m venv "$VENV_DIR"
 
 echo "=== Activando entorno virtual ==="
 source "$VENV_DIR/bin/activate"
 
-echo "=== Instalando dependencias de $REQ_FILE ==="
+echo "=== Actualizando pip e instalando dependencias ==="
 pip install --upgrade pip
 pip install -r "$REQ_FILE"
 
-
+echo
+echo "=== Entorno virtual creado ==="
+echo "Para activarlo manualmente luego:"
+echo "  source $VENV_DIR/bin/activate"
